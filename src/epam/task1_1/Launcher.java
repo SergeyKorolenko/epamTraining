f64057eb6757e6b162f9df5180387ff589b9234a
Сергей <@Sergey>
2019-07-05 00:47:46 +0300

    update 4

--- src/epam/task1_1/Launcher.java
+++ src/epam/task1_1/Launcher.java
@@ -5,6 +5,14 @@ public class Launcher {
 	private static final int N = 4; // constant for the fourth task
 	private static final int AMOUNT_OF_NUMERALS = 6; // constant for the fifth task
 	private static final int COUNT_OF_STEPS = 7; // constant for the sixth task
+	private static final int MIN_FOUR_DIGIT_NUMBER = 1000;
+	private static final int MAX_FOUR_DIGIT_NUMBER = 9999;
+	private static final int MIN_SIX_DIGIT_NUMBER = 100000;
+	private static final int MAX_SIX_DIGIT_NUMBER = 999999;
+	private static final int MIN_SEVEN_DIGIT_NUMBER = 1000000;
+	private static final int MAX_SEVEN_DIGIT_NUMBER = 9999999;
+	private static final int DIVIDER = 10;
+	private static final int FACTOR = 1000000;
 	
 	// The first task
 	public static int checkNumbers(double a, double b, double c) {
@@ -28,6 +36,10 @@ public class Launcher {
 	}
 	
 	// The second task
+	public static boolean isPositive(double weightKilogram) {
+		return weightKilogram > 0 ? true : false;
+	}
+	
 	public static void transferWeight(double weightKilogram) {
 		System.out.println("Dinosaur weight in milligrams is : " + weightKilogram * 1000000 + " milligrams.");
 		System.out.println("Dinosaur weight in grams is : " + weightKilogram * 1000 + " grams.");
@@ -41,12 +53,15 @@ public class Launcher {
 	}
 	
 	// The fourth task
+	private static boolean isFourDigitNumber(int fourDigitNumber){
+		return fourDigitNumber >= MIN_FOUR_DIGIT_NUMBER && fourDigitNumber <= MAX_FOUR_DIGIT_NUMBER ? true : false;
+	}
+	
 	public static void checkSequenceOfNumbers(int fourDigitNumber) {	
 		int []array = new int[N];
-		int step = 10;
 		for(int i = array.length - 1; i >= 0; i--) {
-			array[i] = fourDigitNumber % step;
-			fourDigitNumber /= step;
+			array[i] = fourDigitNumber % DIVIDER;
+			fourDigitNumber /= DIVIDER;
 		}
 		int temp = 0;
 		for(int i = 0; i < array.length - 1 ; i++) {
@@ -57,7 +72,6 @@ public class Launcher {
 				temp--;
 			} 
 		}
-		
 		if(temp == array.length - 1) {
 			System.out.println("Numerals of four digit number form an increasing sequence.");
 		} else if(Math.abs(temp) == array.length - 1) {
@@ -70,14 +84,17 @@ public class Launcher {
 	
 	// The fifth task
 	
+	private static boolean isSixDigitNumber(int sixDigitNumber){
+		return sixDigitNumber >= MIN_SIX_DIGIT_NUMBER && sixDigitNumber <= MAX_SIX_DIGIT_NUMBER ? true : false;
+	}
+	
 	public static void calculateMeansOfNumbers(int number) {
 		int tempNumber = 0;
-		int step = 10;
 		double arithmeticMean = 0;
 		double geometricMean = 1;
 		for(int i = 0; i < AMOUNT_OF_NUMERALS; i++) {
-			tempNumber = number % step;
-			number /= step;
+			tempNumber = number % DIVIDER;
+			number /= DIVIDER;
 			arithmeticMean += tempNumber;
 			geometricMean *= tempNumber;
 		}
@@ -89,15 +106,18 @@ public class Launcher {
 	
 	// The sixth task
 	
+	private static boolean isSevenDigitNumber(int sevenDigitNumber){
+		return sevenDigitNumber >= MIN_SEVEN_DIGIT_NUMBER && sevenDigitNumber <= MAX_SEVEN_DIGIT_NUMBER ? true : false;
+	}
+	
 	public static void reversNumbers(int number){
 		int reversNumber = 0;
-		int step = 10;
 		int i = 0;
-		int factor = 1000000;
+		int factor = FACTOR;
 		while(i < COUNT_OF_STEPS){
-			reversNumber += (number % step) * factor;
-			number /= step;
-			factor /= step;
+			reversNumber += (number % DIVIDER) * factor;
+			number /= DIVIDER;
+			factor /= DIVIDER;
 			i++;
 		}
 		 System.out.print("Revers number is : " + reversNumber + ".");
@@ -123,28 +143,37 @@ public class Launcher {
 		// The second task
 		double weightKilogram = 50.5; 
 		
-		transferWeight(weightKilogram);
+		if(isPositive(weightKilogram)) {
+			transferWeight(weightKilogram);
+		}
 		
 		// The third task
 		double r1 = 4.5;
 		double r2 = 2.2; 
-		
-		findRingSquare(r1, r2);
+		if(isPositive(r1) && isPositive(r2)) {
+			findRingSquare(r1, r2);
+		}
 		
 		// The fourth task
 		int forDigitNumber = 1234; 
 		
-		checkSequenceOfNumbers(forDigitNumber);
+		if(isFourDigitNumber(forDigitNumber)) {
+			checkSequenceOfNumbers(forDigitNumber);
+		}
 		
 		// The fifth task
 		int sixDigitNumber = 123456;
 		
-		calculateMeansOfNumbers(sixDigitNumber);
+		if(isSixDigitNumber(sixDigitNumber)) {
+			calculateMeansOfNumbers(sixDigitNumber);
+		}
 		
 		// The sixth task
 		int sevenDigitNumber = 1234567;
 		
-		reversNumbers(sevenDigitNumber);
+		if(isSevenDigitNumber(sevenDigitNumber)) {
+			reversNumbers(sevenDigitNumber);
+		}
 		
 		// The seventh task
 		int d = 5;
